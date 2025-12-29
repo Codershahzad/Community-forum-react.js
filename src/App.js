4f79578fa6bfd9922563fbafccca08962d1949c8
codershahzad <connectshahzadon@gmail.com>
2025-12-30 03:48:01 +0500

    working is complete

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import React from "react";
+import { HashRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { auth } from "./firebase";
 
@@ -14,16 +15,16 @@ export default function App() {
   const [user] = useAuthState(auth);
 
   return (
-    <BrowserRouter>
+    <Router>
       <Navbar user={user} />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Home user={user} />} />
         <Route path="/login" element={!user ? <Login /> : <Navigate to="/" />} />
         <Route path="/signup" element={!user ? <Signup /> : <Navigate to="/" />} />
         <Route path="/profile" element={user ? <Profile /> : <Navigate to="/login" />} />
         <Route path="/ask" element={user ? <AskQuestion /> : <Navigate to="/login" />} />
         <Route path="/question/:id" element={<QuestionDetails user={user} />} />
       </Routes>
-    </BrowserRouter>
+    </Router>
   );
 }
